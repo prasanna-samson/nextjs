1a503c33aed76e675805f84e251a126685ac9a6d
prasanna <prasanna.xyloinc.2024@gmail.com>
2024-07-25 15:03:19 +0530

    created new file

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='ffixed inset-0 bottom-0 flex flex-col items-center bg-neutral-900'>
+
+<div className='w-full p-4 text-center text-white'>
+@ 2023 JVLcode
+
+</div>
+
+    </div>
+  )
+}
+
+export default Footer
